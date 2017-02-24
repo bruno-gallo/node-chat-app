d514f78f1a600724628bb4b44ce1e57c0a26afe5
Tangocode <tangocode@MacBook-Air-de-Tangocode.local>
2017-02-24 00:15:27 -0300

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -32,12 +32,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
     // Previene que la página se vuelva a cargar, lo cual es el comportamiento por defecto
     e.preventDefault();
+    var messageTextbox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
         from: "User",
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('');
     });
 });
 
@@ -47,12 +48,17 @@ locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser');
     }
+
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
-        })
+        });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location');
     });
 });
